a6da4a62c2383d66b1dd42c519318554e38e2a2c
Albert <albertvalldeperas@gmail.com>
2021-12-23 18:42:43 +0100

    avs06 | PAC2 | final version for exercice 3.1

--- semaphore3-1.c
+++ semaphore3-1.c
@@ -7,17 +7,14 @@
 int main(int argc, char *argv[])
 {
     sem_t sem1;
-    int semVal;
 
-    sem_init(&sem1, 0, 10);
+    sem_init(&sem1, 0, 10); // create semaphore with initial value of 10
 
     for (int i = 0; i < W_TIMES; i++) sem_wait(&sem1);
 
     for (int i = 0; i < P_TIMES; i++) sem_post(&sem1);
 
-    sem_getvalue(&sem1, &semVal);
-
-    printf("Current semaphore value is %d\n", semVal);
+    printf("Current semaphore value is %d\n", sem_getvalue(&sem1, &semVal));
 
     return 0;
 }
